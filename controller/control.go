fcc11146e9326b5891215052fc1aa282384b174b
tarxvrf <tarxvrf@gmail.com>
2025-07-24 09:39:13 +0700

    perbaikan conte

--- controller/control.go
+++ controller/control.go
@@ -4,6 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 func HandleControl(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
-		"message": "ini kenapa dtang iwtku",
+		"message": "ini kenapa dtang ini bang",
 	})
 }
